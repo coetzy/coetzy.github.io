5e1d23f04eecf224158106aa68397b5f1707fffa
coetzy <88256728+coetzy@users.noreply.github.com>
2022-05-24 02:36:35 -0300

    Update imagedb

--- src/imagedb.tsx
+++ src/imagedb.tsx
@@ -2,158 +2,472 @@ type A = { [key: string]: any };
 
 const imagedb: { [key: string]: any } = {
   murales: [
-    {
-      path: "./images/murales/1.jpg",
-      title: "Rambla, parada 21 de la mansa, Maldonado.",
-      collection: "",
-    },
-    {
-      path: "./images/murales/2.jpg",
-      title: "Venecia y Roque Saenz Peña, Ciudad de la Costa.",
-      collection: "",
-    },
+   
+    
     {
       path: "./images/murales/3.jpg",
       title: "Marco Bruto y 26 de Marzo, Montevideo.",
-      collection: "",
+      collection: "Montevideo",
     },
     {
       path: "./images/murales/4.jpg",
       title: "Nicaragua y General Rondeau, Montevideo.",
-      collection: "",
+      collection: "Montevideo",
     },
     {
       path: "./images/murales/5.jpg",
       title: "Ellauri y Bulevar España, Montevideo.",
-      collection: "",
+      collection: "Montevideo",
+    },
+    {
+      path: "./images/murales/1.jpg",
+      title: "Rambla, parada 21 de la mansa, Maldonado.",
+      collection: "Maldonado",
     },
     {
       path: "./images/murales/6.jpg",
       title: "Veterinaria Barrios Jr, Maldonado.",
-      collection: "",
+      collection: "Maldonado",
     },
     {
       path: "./images/murales/7.jpg",
       title: "En Estadía Pocitos, Montevideo.",
-      collection: "",
+      collection: "Montevideo",
     },
     {
       path: "./images/murales/8.jpg",
-      title: "Canelones y Torres García, Las Piedras",
-      collection: "",
+      title: "Canelones y Torres García, Las Piedras.",
+      collection: "Las Piedras",
     },
     {
       path: "./images/murales/9.jpg",
       title: "Torres García, Las Piedras.",
-      collection: "",
+      collection: "Las Piedras",
     },
     {
       path: "./images/murales/10.jpg",
       title: "Minas y Miguelete, Montevideo.",
-      collection: "",
+      collection: "Montevideo",
     },
     {
       path: "./images/murales/11.jpg",
       title: "Veterinaria Libia sucursal de Malvin, Montevideo.",
-      collection: "",
+      collection: "Montevideo",
     },
     {
       path: "./images/murales/12.jpg",
       title: "25 de Mayo y Colón, Montevideo.",
-      collection: "",
+      collection: "Montevideo",
+    },
+    {
+      path: "./images/murales/2.jpg",
+      title: "Venecia y Roque Saenz Peña, Ciudad de la Costa.",
+      collection: "Ciudad de la Costa",
     },
   ],
   pinturas: [
     {
-      path: "./images/murales/1.jpg",
+      path: "./images/Pinturas/acua/01.jpg",
       title: "Marco Bruto",
       collection: "acuarelas",
       height: 30,
     },
     {
-      path: "./images/murales/2.jpg",
-      title: "Marco ",
+      path: "./images/Pinturas/acua/1.jpg",
+      title: "",
       collection: "acuarelas",
       height: 50,
     },
     {
-      path: "./images/murales/3.jpg",
-      title: "Bruto",
+      path: "./images/Pinturas/acua/2.jpg",
+      title: "",
       collection: "acuarelas",
       height: 40,
     },
     {
-      path: "./images/fotografia/1_002.jpg",
-      title: "Marco Bruto",
-      collection: "lapices",
+      path: "./images/Pinturas/acrilico/01.png",
+      title: "",
+      collection: "acrilícos",
       height: 60,
     },
     {
-      path: "./images/fotografia/1_003.jpg",
-      title: "Marco ",
-      collection: "lapices",
+      path: "./images/Pinturas/acrilico/02.jpg",
+      title: "",
+      collection: "acrilícos",
       height: 58,
     },
     {
-      path: "./images/fotografia/32.jpg",
-      title: "Marco ",
+      path: "./images/Pinturas/acrilico/03.png",
+      title: " ",
+      collection: "acrilícos",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/acrilico/04.jpg",
+      title: "",
+      collection: "acrilícos",
+      height: 60,
+    },
+    {
+      path: "./images/Pinturas/acrilico/05.jpg",
+      title: " ",
+      collection: "acrilícos",
+      height: 58,
+    },
+    {
+      path: "./images/Pinturas/acrilico/06.jpg",
+      title: "",
+      collection: "acrilícos",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/acrilico/07.jpg",
+      title: " ",
+      collection: "acrilícos",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/acrilico/08.jpg",
+      title: " ",
+      collection: "acrilícos",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/acrilico/09.jpg",
+      title: " ",
+      collection: "acrilícos",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/acrilico/10.jpg",
+      title: " ",
+      collection: "acrilícos",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/acrilico/11.png",
+      title: " ",
+      collection: "acrilícos",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/acrilico/12.png",
+      title: " ",
+      collection: "acrilícos",
+      height: 50,
+    },
+
+    {
+      path: "./images/Pinturas/acrilico/13.jpg",
+      title: "",
+      collection: "acrilícos",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/dripens/16.jpg",
+      title: "",
+      collection: "dripens",
+      height: 50,
+    },
+
+    {
+      path: "./images/Pinturas/dripens/17.jpg",
+      title: " ",
+      collection: "dripens",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/lapices/04.jpg",
+      title: " ",
+      collection: "lapices",
+      height: 50,
+    },
+
+    {
+      path: "./images/Pinturas/lapices/06.jpg",
+      title: "",
+      collection: "lapices",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/tinta/1.jpg",
+      title: " ",
+      collection: "tinta",
+      height: 50,
+    },
+
+    {
+      path: "./images/Pinturas/tinta/2.jpg",
+      title: "",
+      collection: "tinta",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/tinta/3.jpg",
+      title: "",
+      collection: "tinta",
+      height: 50,
+    },
+    {
+      path: "./images/Pinturas/tinta/4.jpg",
+      title: "",
       collection: "tinta",
       height: 50,
     },
   ],
-  hue: [
+  Fotografías: [
     {
-      path: "./images/fotografia/1_002.jpg",
-      title: "Marco Bruto",
-      collection: "analógicas",
+      path: "./images/fotografia/tunel/30.jpg",
+      title: "",
+      collection: "Túnel",
     },
     {
-      path: "./images/fotografia/1_003.jpg",
-      title: "Marco ",
-      collection: "analógicas",
+      path: "./images/fotografia/tunel/31.jpg",
+      title: "",
+      collection: "Túnel",
     },
     {
-      path: "./images/fotografia/1_003.jpg",
-      title: "Marco ",
-      collection: "analógicas",
+      path: "./images/fotografia/tunel/32.jpg",
+      title: "",
+      collection: "Túnel",
     },
     {
-      path: "./images/fotografia/1_002.jpg",
-      title: "Marco Bruto",
-      collection: "analógicas",
+      path: "./images/fotografia/tunel/33.jpg",
+      title: "",
+      collection: "Túnel",
+    },
+
+    {
+      path: "./images/fotografia/paz/1.jpg",
+      title: "",
+      collection: "Paz",
     },
     {
-      path: "./images/fotografia/1_002.jpg",
-      title: "Marco Bruto",
-      collection: "nosé",
+      path: "./images/fotografia/paz/2.jpg",
+      title: "",
+      collection: "Paz",
     },
     {
-      path: "./images/fotografia/1_003.jpg",
-      title: "Marco ",
-      collection: "nosé",
+      path: "./images/fotografia/paz/3.jpg",
+      title: "",
+      collection: "Paz",
     },
     {
-      path: "./images/fotografia/32.jpg",
-      title: "Marco ",
-      collection: "digitales",
+      path: "./images/fotografia/paz/4.jpg",
+      title: "",
+      collection: "Paz",
     },
+
+    {
+      path: "./images/fotografia/paz/5.jpg",
+      title: "",
+      collection: "Paz",
+    },
+    {
+      path: "./images/fotografia/paz/6.jpg",
+      title: "",
+      collection: "Paz",
+    },
+  
+    {
+      path: "./images/fotografia/maison/1.jpg",
+      title: "",
+      collection: "Maison",
+    },
+    {
+      path: "./images/fotografia/maison/2.jpg",
+      title: "",
+      collection: "Maison",
+    },
+    {
+      path: "./images/fotografia/maison/3.jpg",
+      title: "",
+      collection: "Maison",
+    },
+    {
+      path: "./images/fotografia/maison/4.jpg",
+      title: "",
+      collection: "Maison",
+    },
+    {
+      path: "./images/fotografia/maison/5.jpg",
+      title: "",
+      collection: "Maison",
+    },
+    {
+      path: "./images/fotografia/maison/6.jpg",
+      title: "",
+      collection: "Maison",
+    },
+    {
+      path: "./images/fotografia/maison/7.jpg",
+      title: "",
+      collection: "Maison",
+    },
+    {
+      path: "./images/fotografia/maison/8.jpg",
+      title: "",
+      collection: "Maison",
+    },
+
+
+
+    {
+      path: "./images/fotografia/graffiti/1.JPG",
+      title: "",
+      collection: "Graffiti",
+    },
+    {
+      path: "./images/fotografia/graffiti/2.JPG",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/3.JPG",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/4.JPG",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/5.JPG",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/6.jpg",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/7.JPG",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/8.JPG",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/9.JPG",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/10.jpg",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/11.jpg",
+      title: "",
+      collection: "Graffiti",
+    }, {
+      path: "./images/fotografia/graffiti/12.JPG",
+      title: "",
+      collection: "Graffiti",
+    },{
+      path: "./images/fotografia/graffiti/13.jpg",
+      title: "",
+      collection: "Graffiti",
+    },{
+      path: "./images/fotografia/graffiti/14.jpg",
+      title: "",
+      collection: "Graffiti",
+    },{
+      path: "./images/fotografia/graffiti/15.JPG",
+      title: "",
+      collection: "Graffiti",
+    },{
+      path: "./images/fotografia/graffiti/16.JPG",
+      title: "",
+      collection: "Graffiti",
+    },{
+      path: "./images/fotografia/graffiti/17.JPG",
+      title: "",
+      collection: "Graffiti",
+    },{
+      path: "./images/fotografia/graffiti/18.JPG",
+      title: "",
+      collection: "Graffiti",
+    },{
+      path: "./images/fotografia/graffiti/19.JPG",
+      title: "",
+      collection: "Graffiti",
+    },{
+      path: "./images/fotografia/graffiti/20.jpg",
+      title: "",
+      collection: "Graffiti",
+    },
+  {
+    path: "./images/fotografia/graffiti/21.jpg",
+    title: "",
+    collection: "Graffiti",
+  },{
+    path: "./images/fotografia/graffiti/22.jpg",
+    title: "",
+    collection: "Graffiti",
+  },
+
+
+  
+  {
+    path: "./images/fotografia/processing/1.png",
+    title: "",
+    collection: "Altering",
+  },
+  {
+    path: "./images/fotografia/processing/2.png",
+    title: "",
+    collection: "Altering",
+  },
+  {
+    path: "./images/fotografia/processing/3.png",
+    title: "",
+    collection: "Altering",
+  },
+  {
+    path: "./images/fotografia/processing/4.jpg",
+    title: "",
+    collection: "Altering",
+  },
+  {
+    path: "./images/fotografia/processing/5.jpg",
+    title: "",
+    collection: "Altering",
+  },
+  {
+    path: "./images/fotografia/processing/6.jpg",
+    title: "",
+    collection: "Altering",
+  },
+  {
+    path: "./images/fotografia/processing/7.jpg",
+    title: "",
+    collection: "Altering",
+  },
+  {
+    path: "./images/fotografia/processing/8.png",
+    title: "",
+    collection: "Altering",
+  },
+  {
+    path: "./images/fotografia/processing/9.png",
+    title: "",
+    collection: "Altering",
+  },
+
+
+
+
   ],
   animaciones: [
     {
-      path: "./images/murales/1.jpg",
+      path: "./images/animacion/2.mp4",
       title: "Rambla, parada 21 de la mansa, Maldonado.",
       collection: "",
     },
     {
-      path: "./images/murales/2.jpg",
+      path: "./images/animacion/02.mp4",
       title: "Venecia y Roque Saenz Peña, Ciudad de la Costa.",
       collection: "",
     },
-    {
-      path: "./images/murales/3.jpg",
-      title: "Marco Bruto y 26 de Marzo, Montevideo.",
-      collection: "",
-    },
+    
   ],
 };
 
