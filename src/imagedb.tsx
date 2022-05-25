46f9ef6010fe140267fa954a1e39919831b6a211
Julio Morero <julio@tryolabs.com>
2022-05-24 21:50:23 -0300

    Fix imagedb

--- src/imagedb.tsx
+++ src/imagedb.tsx
@@ -1,5 +1,3 @@
-type A = { [key: string]: any };
-
 const imagedb: { [key: string]: any } = {
   murales: [
     {
@@ -66,390 +64,315 @@ const imagedb: { [key: string]: any } = {
   pinturas: [
     {
       path: "./images/Pinturas/acua/01.jpg",
-      title: "",
-      collection: "acuarelas",
+      collection: "Acuarelas",
     },
     {
       path: "./images/Pinturas/acua/1.jpg",
-      title: "",
-      collection: "acuarelas",
+      collection: "Acuarelas",
     },
     {
       path: "./images/Pinturas/acua/2.jpg",
-      title: "",
-      collection: "acuarelas",
+      collection: "Acuarelas",
     },
     {
       path: "./images/Pinturas/acrilico/01.png",
-      title: "",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/02.jpg",
-      title: "",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/03.png",
-      title: " ",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/04.jpg",
-      title: "",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/05.jpg",
-      title: " ",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/06.jpg",
-      title: "",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/07.jpg",
-      title: " ",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/08.jpg",
-      title: " ",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/09.jpg",
-      title: " ",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/10.jpg",
-      title: " ",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/11.png",
-      title: " ",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/acrilico/12.png",
-      title: " ",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
 
     {
       path: "./images/Pinturas/acrilico/13.jpg",
-      title: "",
-      collection: "acrilícos",
+      collection: "Acrilícos",
     },
     {
       path: "./images/Pinturas/dripens/16.jpg",
-      title: "",
-      collection: "dripens",
+      collection: "Dripens",
     },
 
     {
       path: "./images/Pinturas/dripens/17.jpg",
-      title: " ",
-      collection: "dripens",
+      collection: "Dripens",
     },
     {
       path: "./images/Pinturas/lapices/04.jpg",
-      title: " ",
-      collection: "lapices",
+      collection: "Lapices",
     },
 
     {
       path: "./images/Pinturas/lapices/06.jpg",
-      title: "",
-      collection: "lapices",
+      collection: "Lapices",
     },
     {
       path: "./images/Pinturas/tinta/1.jpg",
-      title: " ",
-      collection: "tinta",
+      collection: "Tinta",
     },
 
     {
       path: "./images/Pinturas/tinta/2.jpg",
-      title: "",
-      collection: "tinta",
+      collection: "Tinta",
     },
     {
       path: "./images/Pinturas/tinta/3.jpg",
-      title: "",
-      collection: "tinta",
+      collection: "Tinta",
     },
     {
       path: "./images/Pinturas/tinta/4.jpg",
-      title: "",
-      collection: "tinta",
+      collection: "Tinta",
     },
   ],
-  Fotografías: [
+  fotografías: [
     {
       path: "./images/fotografia/tunel/30.jpg",
-      title: "",
       collection: "Túnel",
     },
     {
       path: "./images/fotografia/tunel/31.jpg",
-      title: "",
       collection: "Túnel",
     },
     {
       path: "./images/fotografia/tunel/32.jpg",
-      title: "",
       collection: "Túnel",
     },
     {
       path: "./images/fotografia/tunel/33.jpg",
-      title: "",
       collection: "Túnel",
     },
 
     {
       path: "./images/fotografia/paz/1.jpg",
-      title: "",
       collection: "Paz",
     },
     {
       path: "./images/fotografia/paz/2.jpg",
-      title: "",
       collection: "Paz",
     },
     {
       path: "./images/fotografia/paz/3.jpg",
-      title: "",
       collection: "Paz",
     },
     {
       path: "./images/fotografia/paz/4.jpg",
-      title: "",
       collection: "Paz",
     },
 
     {
       path: "./images/fotografia/paz/5.jpg",
-      title: "",
       collection: "Paz",
     },
     {
       path: "./images/fotografia/paz/6.jpg",
-      title: "",
       collection: "Paz",
     },
 
     {
       path: "./images/fotografia/maison/1.jpg",
-      title: "",
       collection: "Maison",
     },
     {
       path: "./images/fotografia/maison/2.jpg",
-      title: "",
       collection: "Maison",
     },
     {
       path: "./images/fotografia/maison/3.jpg",
-      title: "",
       collection: "Maison",
     },
     {
       path: "./images/fotografia/maison/4.jpg",
-      title: "",
       collection: "Maison",
     },
     {
       path: "./images/fotografia/maison/5.jpg",
-      title: "",
       collection: "Maison",
     },
     {
       path: "./images/fotografia/maison/6.jpg",
-      title: "",
       collection: "Maison",
     },
     {
       path: "./images/fotografia/maison/7.jpg",
-      title: "",
       collection: "Maison",
     },
     {
       path: "./images/fotografia/maison/8.jpg",
-      title: "",
       collection: "Maison",
     },
 
     {
       path: "./images/fotografia/graffiti/1.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/2.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/3.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/4.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/5.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/6.jpg",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/7.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/8.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/9.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/10.jpg",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/11.jpg",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/12.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/13.jpg",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/14.jpg",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/15.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/16.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/17.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/18.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/19.JPG",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/20.jpg",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/21.jpg",
-      title: "",
       collection: "Graffiti",
     },
     {
       path: "./images/fotografia/graffiti/22.jpg",
-      title: "",
       collection: "Graffiti",
     },
 
     {
       path: "./images/fotografia/processing/1.png",
-      title: "",
       collection: "Altering",
     },
     {
       path: "./images/fotografia/processing/2.png",
-      title: "",
       collection: "Altering",
     },
     {
       path: "./images/fotografia/processing/3.png",
-      title: "",
       collection: "Altering",
     },
     {
       path: "./images/fotografia/processing/4.jpg",
-      title: "",
       collection: "Altering",
     },
     {
       path: "./images/fotografia/processing/5.jpg",
-      title: "",
       collection: "Altering",
     },
     {
       path: "./images/fotografia/processing/6.jpg",
-      title: "",
       collection: "Altering",
     },
     {
       path: "./images/fotografia/processing/7.jpg",
-      title: "",
       collection: "Altering",
     },
     {
       path: "./images/fotografia/processing/8.png",
-      title: "",
       collection: "Altering",
     },
     {
       path: "./images/fotografia/processing/9.png",
-      title: "",
       collection: "Altering",
     },
   ],
   animaciones: [
     {
       path: "./images/animacion/2.mp4",
-      title: "",
       collection: "",
     },
     {
       path: "./images/animacion/02.mp4",
-      title: "",
       collection: "",
     },
   ],
