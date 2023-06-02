9f0ee344016aa5313a9e6181a73a90b15df1cfc5
Julio Morero <julio@tryolabs.com>
2023-06-02 15:15:02 -0300

    Separate about paragraphs with line breaks

--- src/About.tsx
+++ src/About.tsx
@@ -3,6 +3,8 @@ import { useData } from "./api";
 const About = () => {
   const { data } = useData();
 
+  let paragraphs = data?.about.description.split("\n\n")
+
   return (
     <main className="flex flex-1 flex-col lg:flex-row lg:justify-center overflow-auto items-center m-8">
       <img
@@ -10,7 +12,8 @@ const About = () => {
         src={data && data.about.img}
       ></img>
       <div className="p-4 lg:w-1/2 text-lg">
-        <p>{data && data.about.description}</p>
+        {paragraphs?.slice(0,-1).map(par => <><p>{par}</p><br/></>)}
+        <p>{paragraphs && paragraphs.at(-1)}</p>
       </div>
     </main>
   );
